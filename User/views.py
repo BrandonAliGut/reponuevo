65b87269556ac3a43bec458aaefea7e116ab2a04
Brandon <bg4710699@gmail.com>
2023-09-11 22:44:49 -0600

    subida

--- User/views.py
+++ User/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def index(request):
     print("hello sisteme Malo")
-    return render(request, 'index.html', {})
+    return render(request, 'Index.html', {})
